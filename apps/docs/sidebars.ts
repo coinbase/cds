2ebf03a75a9780aa26d5c324cf2933bddae739fd
Maximo Macchi <232606069+maximo-macchi-cb@users.noreply.github.com>
2025-09-30 10:09:33 -0500

    Initial docs content

--- apps/docs/sidebars.ts
+++ apps/docs/sidebars.ts
@@ -187,6 +187,11 @@ const sidebars: SidebarsConfig = {
               id: 'components/inputs/RadioGroup/radioGroup',
               label: 'RadioGroup',
             },
+            {
+              type: 'doc',
+              id: 'components/inputs/SelectAlpha/select-alpha',
+              label: 'Select (Alpha)',
+            },
             { type: 'doc', id: 'components/inputs/Select/select', label: 'Select' },
             {
               type: 'doc',
