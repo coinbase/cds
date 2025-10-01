7009aaf49951ba16d51bc7c252bc4094a8223c8b
Hunter Copp <huntercolecopp@gmail.com>
2025-10-01 10:10:45 -0400

    feat: hide components on mobile

--- apps/docs/src/components/page/ComponentHeader/index.tsx
+++ apps/docs/src/components/page/ComponentHeader/index.tsx
@@ -110,7 +110,7 @@ export const ComponentHeader = memo(
 
     return (
       <VStack background="bgAlternate" borderRadius={600} overflow="hidden" width="100%">
-        <VStack height={200} width="100%">
+        <VStack display={{ base: 'flex', phone: 'none' }} height={200} width="100%">
           {typeof activeBanner === 'string' ? (
             <img
               alt={`${title} banner`}
