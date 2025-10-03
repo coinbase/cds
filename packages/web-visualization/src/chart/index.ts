27b3e67b463a32e9e2cc5fd496c098b27cd42eaf
Hunter Copp <huntercolecopp@gmail.com>
2025-10-03 15:24:06 -0400

    Update lint for doc site

--- packages/web-visualization/src/chart/index.ts
+++ packages/web-visualization/src/chart/index.ts
@@ -1,13 +1,14 @@
 // codegen:start {preset: barrel, include: [./*.tsx, ./*/index.ts]}
-export * from './area';
-export * from './axis';
-export * from './bar';
+export * from './area/index';
+export * from './axis/index';
+export * from './bar/index';
 export * from './CartesianChart';
 export * from './ChartProvider';
-export * from './line';
+export * from './line/index';
 export * from './Path';
 export * from './PeriodSelector';
 export * from './Point';
-export * from './scrubber';
-export * from './text';
+export * from './scrubber/index';
+export * from './text/index';
+export * from './utils/index';
 // codegen:end
