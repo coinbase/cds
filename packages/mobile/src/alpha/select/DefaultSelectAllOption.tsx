587ab73bd5a02c7bfb6a57f992371ae9f85b90ef
Maximo Macchi <232606069+maximo-macchi-cb@users.noreply.github.com>
2025-10-08 17:49:11 -0500

    WIP on adding generic types to mobile

--- packages/mobile/src/alpha/select/DefaultSelectAllOption.tsx
+++ packages/mobile/src/alpha/select/DefaultSelectAllOption.tsx
@@ -3,43 +3,43 @@ import { memo } from 'react';
 import { Divider } from '../../layout/Divider';
 
 import { DefaultSelectOption } from './DefaultSelectOption';
-import { type SelectOptionComponent } from './Select';
+import { type SelectOptionComponent, type SelectType } from './Select';
 
-export const DefaultSelectAllOption: SelectOptionComponent<'single' | 'multi'> = memo(
-  ({
-    accessory,
-    blendStyles,
-    compact,
-    detail,
-    disabled,
-    label,
-    media,
-    onPress,
-    selected,
-    style,
-    type,
-    styles,
-  }) => {
-    return (
-      <>
-        <DefaultSelectOption
-          key="select-all"
-          accessory={accessory}
-          blendStyles={blendStyles}
-          compact={compact}
-          detail={detail}
-          disabled={disabled}
-          label={label}
-          media={media}
-          onPress={onPress}
-          selected={selected}
-          style={style}
-          styles={styles}
-          type={type}
-          value="select-all"
-        />
-        <Divider paddingX={2} style={styles?.selectAllDivider} />
-      </>
-    );
-  },
-);
+const DefaultSelectAllOptionBase = <Type extends SelectType = 'single', T extends string = string>({
+  accessory,
+  blendStyles,
+  compact,
+  detail,
+  disabled,
+  label,
+  media,
+  onPress,
+  selected,
+  style,
+  type,
+  styles,
+}: Parameters<SelectOptionComponent<Type, T>>[0]) => {
+  return (
+    <>
+      <DefaultSelectOption
+        key="select-all"
+        accessory={accessory}
+        blendStyles={blendStyles}
+        compact={compact}
+        detail={detail}
+        disabled={disabled}
+        label={label}
+        media={media}
+        onPress={onPress}
+        selected={selected}
+        style={style}
+        styles={styles}
+        type={type}
+        value={'select-all'}
+      />
+      <Divider paddingX={2} style={styles?.selectAllDivider} />
+    </>
+  );
+};
+
+export const DefaultSelectAllOption = memo(DefaultSelectAllOptionBase) as SelectOptionComponent;
