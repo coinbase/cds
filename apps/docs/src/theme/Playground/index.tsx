10b9374819898e91600f8f17337e38dde1910dd6
Cody Nova <cody.nova@coinbase.com>
2025-10-20 09:01:54 -0500

    Refactor docs Playground and add Prettier

--- apps/docs/src/theme/Playground/index.tsx
+++ apps/docs/src/theme/Playground/index.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
-import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live';
+import { LiveEditor, LiveError, LivePreview, LiveProvider, withLive } from 'react-live';
 import { Collapsible } from '@coinbase/cds-web/collapsible/Collapsible';
 import { Icon } from '@coinbase/cds-web/icons/Icon';
 import { Box } from '@coinbase/cds-web/layout';
@@ -12,19 +12,24 @@ import { Text } from '@coinbase/cds-web/typography/Text';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import ErrorBoundary from '@docusaurus/ErrorBoundary';
 import { ErrorBoundaryErrorMessageFallback } from '@docusaurus/theme-common';
+import * as estreePlugin from 'prettier/plugins/estree.js';
+import * as typescriptPlugin from 'prettier/plugins/typescript.js';
+import { format } from 'prettier/standalone';
 import { themes as prismThemes } from 'prism-react-renderer';
 
 import { usePlaygroundTheme } from '../Layout/Provider/UnifiedThemeContext';
 
 import styles from './styles.module.css';
 
-type PlaygroundProps = Omit<React.ComponentProps<typeof LiveProvider>, 'transformCode'> & {
-  transformCode?: (val: string) => string;
-  children: string;
-  hideControls?: boolean;
-  hidePreview?: boolean;
-  editorStartsExpanded?: boolean;
-};
+const PlaygroundEditorHeader = memo(() => {
+  return (
+    <Box borderedBottom paddingBottom={0.5} paddingTop={0.75} paddingX={1} width="100%">
+      <Text alignItems="center" color="fgMuted" display="flex" font="label1" userSelect="none">
+        <Icon active color="fgMuted" name="pencil" paddingEnd={0.5} size="xs" /> Live Code
+      </Text>
+    </Box>
+  );
+});
 
 const renderErrorFallback = (params: any) => <ErrorBoundaryErrorMessageFallback {...params} />;
 
@@ -78,16 +83,72 @@ const useGetHeadingText = () => {
   return { editorRef, headingText };
 };
 
+const prettierOptions = {
+  parser: 'typescript',
+  plugins: [estreePlugin, typescriptPlugin] as any,
+};
+
+type PlaygroundControlsProps = {
+  collapsed: boolean;
+  headingText: string;
+  onClickCopy: () => void;
+  onToggleCollapsed: () => void;
+};
+
+const PlaygroundControls = memo(
+  ({ collapsed, headingText, onClickCopy, onToggleCollapsed }: PlaygroundControlsProps) => {
+    return (
+      <HStack alignItems="center" gap={2} paddingTop={0.5}>
+        <Pressable
+          noScaleOnPress
+          accessibilityLabel={`${collapsed ? 'Show' : 'Hide'} code${
+            headingText ? ` for ${headingText} example` : ''
+          }`}
+          onClick={onToggleCollapsed}
+        >
+          <HStack alignItems="center">
+            <Icon name={collapsed ? 'caretDown' : 'caretUp'} paddingEnd={0.5} size="xs" />
+            <Text color="fgPrimary" font="label1">
+              {collapsed ? 'Show code' : 'Hide code'}
+            </Text>
+          </HStack>
+        </Pressable>
+        <Pressable
+          noScaleOnPress
+          accessibilityLabel={`Copy code${headingText ? ` for ${headingText} example` : ''}`}
+          onClick={onClickCopy}
+        >
+          <HStack alignItems="center">
+            <Icon name="copy" paddingEnd={0.5} size="xs" />
+            <Text color="fgPrimary" font="label1">
+              Copy code
+            </Text>
+          </HStack>
+        </Pressable>
+      </HStack>
+    );
+  },
+);
+
+type PlaygroundProps = Omit<React.ComponentProps<typeof LiveProvider>, 'transformCode'> & {
+  transformCode?: (val: string) => string;
+  children: string;
+  hideControls?: boolean;
+  hidePreview?: boolean;
+  editorStartsExpanded?: boolean;
+};
+
 const Playground = memo(function Playground({
   children,
   transformCode,
   hideControls,
   hidePreview,
   editorStartsExpanded,
+  code: codeProp,
   ...props
 }: PlaygroundProps): JSX.Element {
-  const initialCode = children.replace(/\n$/, '');
-  const codeRef = useRef(initialCode);
+  const [code, setCode] = useState((codeProp ?? children ?? '').replace(/\n$/, ''));
+  const codeRef = useRef(code);
 
   const [collapsed, setIsCollapsed] = useState(!editorStartsExpanded);
   const toggleCollapsed = useCallback(() => setIsCollapsed((collapsed) => !collapsed), []);
@@ -98,14 +159,16 @@ const Playground = memo(function Playground({
 
   const { editorRef, headingText } = useGetHeadingText();
 
-  // We treat the LiveProvider like an uncontrolled component, but
-  // we want to be able to copy the edited code so we need to keep
-  // track of the code in a ref
+  const handleCodeChange = useCallback((code: string) => {
+    codeRef.current = code;
+    setCode(code);
+  }, []);
+
   const handleTransformCode = useCallback(
     (code: string) => {
-      codeRef.current = code;
-      if (transformCode) return transformCode(code);
-      return code;
+      const transformedCode = transformCode ? transformCode(code) : code;
+      codeRef.current = transformedCode;
+      setCode(transformedCode);
     },
     [transformCode],
   );
@@ -117,15 +180,21 @@ const Playground = memo(function Playground({
       .catch(() => toast.show('Failed to copy to clipboard'));
   }, [toast]);
 
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === 'KeyS' && (event.ctrlKey || event.metaKey)) {
+        event.preventDefault();
+        format(codeRef.current, prettierOptions).then(handleTransformCode);
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, [handleTransformCode]);
+
   return (
     <VStack ref={editorRef} paddingBottom={3} position="relative" zIndex={0}>
       <ThemeProvider activeColorScheme={colorScheme} theme={theme}>
-        <LiveProvider
-          code={initialCode}
-          theme={prismTheme}
-          transformCode={handleTransformCode}
-          {...props}
-        >
+        <LiveProvider code={code} theme={prismTheme} {...props}>
           {!hidePreview && (
             <VStack
               background="bg"
@@ -141,49 +210,17 @@ const Playground = memo(function Playground({
           )}
           <Collapsible collapsed={collapsed} paddingBottom={0.5} paddingTop={1}>
             <VStack background="bg" borderRadius={400} overflow="hidden" width="100%">
-              <Box borderedBottom paddingBottom={0.5} paddingTop={0.75} paddingX={1} width="100%">
-                <Text
-                  alignItems="center"
-                  color="fgMuted"
-                  display="flex"
-                  font="label1"
-                  userSelect="none"
-                >
-                  <Icon active color="fgMuted" name="pencil" paddingEnd={0.5} size="xs" /> Live Code
-                </Text>
-              </Box>
-              <LiveEditor className={styles.playgroundEditor} />
+              <PlaygroundEditorHeader />
+              <LiveEditor className={styles.playgroundEditor} onChange={handleCodeChange} />
             </VStack>
           </Collapsible>
           {!hideControls && (
-            <HStack alignItems="center" gap={2} paddingTop={0.5}>
-              <Pressable
-                noScaleOnPress
-                accessibilityLabel={`${collapsed ? 'Show' : 'Hide'} code${
-                  headingText ? ` for ${headingText} example` : ''
-                }`}
-                onClick={toggleCollapsed}
-              >
-                <HStack alignItems="center">
-                  <Icon name={collapsed ? 'caretDown' : 'caretUp'} paddingEnd={0.5} size="xs" />
-                  <Text color="fgPrimary" font="label1">
-                    {collapsed ? 'Show code' : 'Hide code'}
-                  </Text>
-                </HStack>
-              </Pressable>
-              <Pressable
-                noScaleOnPress
-                accessibilityLabel={`Copy code${headingText ? ` for ${headingText} example` : ''}`}
-                onClick={handleCopyToClipboard}
-              >
-                <HStack alignItems="center">
-                  <Icon name="copy" paddingEnd={0.5} size="xs" />
-                  <Text color="fgPrimary" font="label1">
-                    Copy code
-                  </Text>
-                </HStack>
-              </Pressable>
-            </HStack>
+            <PlaygroundControls
+              collapsed={collapsed}
+              headingText={headingText}
+              onClickCopy={handleCopyToClipboard}
+              onToggleCollapsed={toggleCollapsed}
+            />
           )}
         </LiveProvider>
       </ThemeProvider>
