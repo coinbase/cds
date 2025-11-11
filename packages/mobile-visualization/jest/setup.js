3bd70f7e325a195c32d716ddd53abe26e1b82786
Shrey Patel <shrey.patel@coinbase.com>
2025-11-10 17:04:53 -0800

    test:fix

--- packages/mobile-visualization/jest/setup.js
+++ packages/mobile-visualization/jest/setup.js
@@ -2,5 +2,12 @@ jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
   Reanimated.makeMutable = Reanimated.useSharedValue;
 
+  // Add useEvent mock for react-native-gesture-handler v2.24+
+  Reanimated.useEvent = (handler, eventNames, rebuild) => {
+    return {
+      workletEventHandler: () => {},
+    };
+  };
+
   return Reanimated;
 });
