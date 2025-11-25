2708dc19e640d8624cae69e00a1993395772524c
Maximo Macchi <232606069+maximo-macchi-cb@users.noreply.github.com>
2025-11-24 21:24:42 -0800

    Fix padding for compact

--- packages/web/src/alpha/select/DefaultSelectControl.tsx
+++ packages/web/src/alpha/select/DefaultSelectControl.tsx
@@ -356,7 +356,7 @@ const DefaultSelectControlComponent = memo(
                 overflow="auto"
                 paddingTop={labelVariant === 'inside' ? 0 : undefined}
                 paddingX={1}
-                paddingY={labelVariant === 'inside' || compact ? 0.5 : 1.5}
+                paddingY={labelVariant === 'inside' || compact ? 1 : 1.5}
                 style={styles?.controlValueNode}
               >
                 {valueNode}
@@ -400,7 +400,7 @@ const DefaultSelectControlComponent = memo(
               className={classNames?.controlEndNode}
               flexGrow={1}
               paddingX={2}
-              paddingY={labelVariant === 'inside' || compact ? 0.5 : 1.5}
+              paddingY={labelVariant === 'inside' || compact ? 1 : 1.5}
               style={styles?.controlEndNode}
             >
               {customEndNode ? (
