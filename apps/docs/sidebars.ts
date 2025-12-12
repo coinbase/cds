2d72a5146233173d5f7f77ef2f8e28e2ac39e6b9
Maximo Macchi <232606069+maximo-macchi-cb@users.noreply.github.com>
2025-12-12 12:17:29 -0600

    Add alpha metadata

--- apps/docs/sidebars.ts
+++ apps/docs/sidebars.ts
@@ -134,7 +134,7 @@ const sidebars: SidebarsConfig = {
             {
               type: 'doc',
               id: 'components/inputs/Combobox/combobox',
-              label: 'Combobox',
+              label: 'Combobox (Alpha)',
             },
             {
               type: 'doc',
