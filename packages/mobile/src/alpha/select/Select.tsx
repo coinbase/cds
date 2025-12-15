b045244cc4b6b8b6ae50a1d7726962137e1a7d7d
Maximo Macchi <232606069+maximo-macchi-cb@users.noreply.github.com>
2025-12-14 23:26:54 -0600

    Change default a11y labels

--- packages/mobile/src/alpha/select/Select.tsx
+++ packages/mobile/src/alpha/select/Select.tsx
@@ -62,7 +62,7 @@ const SelectBase = memo(
         compact,
         label,
         labelVariant,
-        accessibilityLabel = type === 'multi' ? 'Multi select control' : undefined,
+        accessibilityLabel = 'Select control',
         accessibilityHint,
         accessibilityRoles = defaultAccessibilityRoles,
         selectAllLabel,
