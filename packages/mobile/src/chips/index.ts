c54457edd8ce6d19713f5d20576c8e7a200865b7
Hunter Copp <huntercolecopp@gmail.com>
2025-12-15 13:05:19 -0500

    chore: add barrel files to web/mobile components

---
+++ packages/mobile/src/chips/index.ts
@@ -0,0 +1,7 @@
+export * from './Chip';
+export * from './ChipProps';
+export * from './InputChip';
+export * from './MediaChip';
+export * from './SelectChip';
+export * from './TabbedChips';
+
---
+++ packages/web/src/multi-content-module/index.ts
@@ -0,0 +1,2 @@
+export * from './MultiContentModule';
+
