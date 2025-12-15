4d4facfe528ba629f3dd6d8a456ba6e0659e2612
Hunter Copp <huntercolecopp@gmail.com>
2025-12-15 13:47:01 -0500

    chore: add barrel files to web/mobile components (#251)
    * chore: add barrel files to web/mobile components
    
    * Update changelog
    
    * Update formatting

---
+++ packages/web/src/pagination/index.ts
@@ -0,0 +1,9 @@
+// codegen:start {preset: barrel, include: ./*.tsx, exclude: ./*.stories.tsx}
+export * from './DefaultPaginationEllipsis';
+export * from './DefaultPaginationNavigationButton';
+export * from './DefaultPaginationNavigationTextButton';
+export * from './DefaultPaginationPageButton';
+export * from './Pagination';
+// codegen:end
+
+export * from './usePagination';
