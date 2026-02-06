35f84f49c79a97d105556b4997135e86d2b36783
Hunter Copp <huntercolecopp@gmail.com>
2026-02-06 15:43:35 -0500

    Fix figma code connect

--- packages/web/src/carousel/__figma__/Carousel.figma.tsx
+++ packages/web/src/carousel/__figma__/Carousel.figma.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { figma } from '@figma/code-connect';
+import figma from '@figma/code-connect/react';
 
 import { Carousel, CarouselItem } from '../';
 
@@ -8,18 +7,8 @@ figma.connect(
   'https://www.figma.com/design/k5CtyJccNQUGMI5bI4lJ2g/%E2%9C%A8-CDS-Components?node-id=48671-10433',
   {
     imports: ["import { Carousel, CarouselItem } from '@coinbase/cds-web/carousel'"],
-    props: {
-      hidePagination: figma.boolean('showPagination', {
-        true: false,
-        false: true,
-      }),
-      title: figma.boolean('hideTitle', {
-        true: undefined,
-        false: 'Section header',
-      }),
-    },
-    example: ({ hidePagination, title }) => (
-      <Carousel hidePagination={hidePagination} title={title}>
+    example: () => (
+      <Carousel>
         <CarouselItem id="1">{/* Item content */}</CarouselItem>
         <CarouselItem id="2">{/* Item content */}</CarouselItem>
         <CarouselItem id="3">{/* Item content */}</CarouselItem>
