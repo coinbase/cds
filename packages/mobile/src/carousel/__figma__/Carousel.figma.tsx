a77c3a3435503e9fc8de85966be99415f2bed234
Hunter Copp <huntercolecopp@gmail.com>
2026-02-12 14:11:00 -0500

    Drop nested child properties

--- packages/mobile/src/carousel/__figma__/Carousel.figma.tsx
+++ packages/mobile/src/carousel/__figma__/Carousel.figma.tsx
@@ -12,27 +12,13 @@ figma.connect(
         true: figma.string('title'),
         false: undefined,
       }),
-      hideNavigation: figma.boolean('show navigation', {
-        true: undefined,
-        false: true,
-      }),
       hidePagination: figma.boolean('show pagination', {
         true: undefined,
         false: true,
       }),
-      autoplay: figma.boolean('show autoplay', {
-        true: true,
-        false: undefined,
-      }),
     },
-    example: ({ title, hideNavigation, hidePagination, autoplay }) => (
-      <Carousel
-        autoplay={autoplay}
-        hideNavigation={hideNavigation}
-        hidePagination={hidePagination}
-        paginationVariant="dot"
-        title={title}
-      >
+    example: ({ title, hidePagination }) => (
+      <Carousel hidePagination={hidePagination} paginationVariant="dot" title={title}>
         <CarouselItem id="1">{/* Item content */}</CarouselItem>
         <CarouselItem id="2">{/* Item content */}</CarouselItem>
         <CarouselItem id="3">{/* Item content */}</CarouselItem>
