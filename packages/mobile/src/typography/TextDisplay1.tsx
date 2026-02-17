3e9a69b38b82bd77a7ac267eb72cf5c5a919a356
Boba <ruikun.hao@coinbase.com>
2026-02-17 12:21:50 -0600

    feat: updated deprecation comment for title componnents in mobile to prevent a11y regression

--- packages/mobile/src/typography/TextDisplay1.tsx
+++ packages/mobile/src/typography/TextDisplay1.tsx
@@ -3,13 +3,13 @@ import type { Text as NativeText } from 'react-native';
 
 import { Text, type TextBaseProps, type TextProps } from './Text';
 
-/** @deprecated Use `Text` with `font="display1"` instead. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display1"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
 export type TextDisplay1BaseProps = TextBaseProps;
 
-/** @deprecated Use `Text` with `font="display1"` instead. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display1"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
 export type TextDisplay1Props = TextProps;
 
-/** @deprecated Use `Text` with `font="display1"` instead. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display1"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
 export const TextDisplay1 = memo(
   forwardRef<NativeText, TextDisplay1Props>(
     ({ accessibilityRole = 'header', font = 'display1', ...props }, ref) => (
--- packages/mobile/src/typography/TextDisplay3.tsx
+++ packages/mobile/src/typography/TextDisplay3.tsx
@@ -3,13 +3,13 @@ import type { Text as NativeText } from 'react-native';
 
 import { Text, type TextBaseProps, type TextProps } from './Text';
 
-/** @deprecated Use `Text` with `font="display3"` instead. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display3"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
 export type TextDisplay3BaseProps = TextBaseProps;
 
-/** @deprecated Use `Text` with `font="display3"` instead. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display3"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
 export type TextDisplay3Props = TextProps;
 
-/** @deprecated Use `Text` with `font="display3"` instead. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display3"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
 export const TextDisplay3 = memo(
   forwardRef<NativeText, TextDisplay3Props>(
     ({ accessibilityRole = 'header', font = 'display3', ...props }, ref) => (
