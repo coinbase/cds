2949ab493433b351ec60f1f5c30521833711626f
Boba <ruikun.hao@coinbase.com>
2026-02-18 10:25:01 -0600

    feat: added default accessibilityRole for header in mobile

--- packages/mobile/src/typography/TextDisplay2.tsx
+++ packages/mobile/src/typography/TextDisplay2.tsx
@@ -3,13 +3,13 @@ import type { Text as NativeText } from 'react-native';
 
 import { Text, type TextBaseProps, type TextProps } from './Text';
 
-/** @deprecated Use `Text` with `font="display2"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display2"` instead. This component will be removed in a future major release. */
 export type TextDisplay2BaseProps = TextBaseProps;
 
-/** @deprecated Use `Text` with `font="display2"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display2"` instead. This component will be removed in a future major release. */
 export type TextDisplay2Props = TextProps;
 
-/** @deprecated Use `Text` with `font="display2"` instead. Note: this component defaults `accessibilityRole` to "header". When migrating, add `accessibilityRole="header"` to preserve accessibility behavior. This component will be removed in a future major release. */
+/** @deprecated Use `Text` with `font="display2"` instead. This component will be removed in a future major release. */
 export const TextDisplay2 = memo(
   forwardRef<NativeText, TextDisplay2Props>(
     ({ accessibilityRole = 'header', font = 'display2', ...props }, ref) => (
