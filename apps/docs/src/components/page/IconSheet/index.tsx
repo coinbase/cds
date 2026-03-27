c2f23a699c4eaecad49767c5acaaaf96d7a5f06d
Maximo Macchi <232606069+maximo-macchi-cb@users.noreply.github.com>
2026-03-27 12:06:32 -0500

    Format files

--- apps/docs/src/components/page/IconSheet/index.tsx
+++ apps/docs/src/components/page/IconSheet/index.tsx
@@ -87,7 +87,11 @@ export const IconSheet = ({ title }: { title?: React.ReactNode }) => {
             <Text color="fgMuted" font="body">
               active prop:
             </Text>
-            <Switch accessibilityLabel="Toggle icon active state" checked={showIconActive} onChange={handleActiveCheck} />
+            <Switch
+              accessibilityLabel="Toggle icon active state"
+              checked={showIconActive}
+              onChange={handleActiveCheck}
+            />
           </Box>
           <Box gap={1}>
             <Text color="fgMuted" font="body">
