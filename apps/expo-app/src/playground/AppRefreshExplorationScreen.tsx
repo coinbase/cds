d3804ec477a946a2345eb0681712bdc31e40983e
Erich Kuerschner <erich.kuerschner@coinbase.com>
2026-06-10 10:35:03 -0500

    CDS-2116: Component config

--- apps/expo-app/src/playground/AppRefreshExplorationScreen.tsx
+++ apps/expo-app/src/playground/AppRefreshExplorationScreen.tsx
@@ -1,10 +1,13 @@
 import React, { memo, useCallback, useMemo, useState } from 'react';
+import { ScrollView } from 'react-native';
+import { useTheme } from '@coinbase/cds-mobile';
 import { Accordion, AccordionItem } from '@coinbase/cds-mobile/accordion';
 import { Button } from '@coinbase/cds-mobile/buttons';
 import { MessagingCard } from '@coinbase/cds-mobile/cards';
 import type { ComponentConfig } from '@coinbase/cds-mobile/core/componentConfig';
 import { Pictogram } from '@coinbase/cds-mobile/illustrations';
-import { HStack, VStack } from '@coinbase/cds-mobile/layout';
+import { Box, HStack, VStack } from '@coinbase/cds-mobile/layout';
+import { Avatar } from '@coinbase/cds-mobile/media';
 import { Pressable } from '@coinbase/cds-mobile/system';
 import { ComponentConfigProvider } from '@coinbase/cds-mobile/system/ComponentConfigProvider';
 import { Text } from '@coinbase/cds-mobile/typography/Text';
@@ -85,6 +88,12 @@ function PriceChart() {
 export const AppRefreshExplorationScreen = memo(() => {
   const config: ComponentConfig = useMemo(
     () => ({
+      Avatar: () => {
+        const { activeColorScheme } = useTheme();
+        return {
+          shape: activeColorScheme === 'light' ? 'square' : 'circle',
+        };
+      },
       MessagingCard: (props) => {
         if (props.type === 'nudge') {
           return {
@@ -102,65 +111,74 @@ export const AppRefreshExplorationScreen = memo(() => {
 
   return (
     <ComponentConfigProvider value={config}>
-      <VStack background="bg" flexGrow={1} gap={4} paddingX={2}>
-        <ComponentExploration componentName="Accordion">
-          <Accordion
-            style={{
-              borderWidth: 1,
-              borderColor: '#EEF0F3',
-              borderRadius: '4%',
-              overflow: 'hidden',
-            }}
-          >
-            <AccordionItem
-              itemKey="1"
-              styles={{ panel: { borderTopWidth: 1, borderColor: '#EEF0F3' } }}
-              subtitle="Item 1 subtitle"
-              title="Item 1"
+      <ScrollView>
+        <VStack background="bg" flexGrow={1} gap={4} paddingBottom={8} paddingX={2}>
+          <ComponentExploration componentName="Accordion">
+            <Accordion
+              style={{
+                borderWidth: 1,
+                borderColor: '#EEF0F3',
+                borderRadius: '4%',
+                overflow: 'hidden',
+              }}
             >
-              <HStack justifyContent="space-between">
-                <Text font="body">TSLA $410 Call 4/20</Text>
-                <Text font="body">
-                  $600.00 (
-                  <Text color="fgPositive" font="body">
-                    ↗ 2.12%
+              <AccordionItem
+                itemKey="1"
+                styles={{ panel: { borderTopWidth: 1, borderColor: '#EEF0F3' } }}
+                subtitle="Item 1 subtitle"
+                title="Item 1"
+              >
+                <HStack justifyContent="space-between">
+                  <Text font="body">TSLA $410 Call 4/20</Text>
+                  <Text font="body">
+                    $600.00 (
+                    <Text color="fgPositive" font="body">
+                      ↗ 2.12%
+                    </Text>
+                    )
                   </Text>
-                  )
+                </HStack>
+              </AccordionItem>
+            </Accordion>
+          </ComponentExploration>
+          <ComponentExploration componentName="Line Chart">
+            <PriceChart />
+          </ComponentExploration>
+          <ComponentExploration componentName="Messaging Card">
+            <MessagingCard
+              action={
+                <Pressable>
+                  <Text color="fg" font="headline">
+                    Get started
+                  </Text>
+                </Pressable>
+              }
+              description={
+                <Text color="fgMuted" font="body">
+                  Get a USDC loan by using your Bitcoin as collateral
                 </Text>
-              </HStack>
-            </AccordionItem>
-          </Accordion>
-        </ComponentExploration>
-        <ComponentExploration componentName="Line Chart">
-          <PriceChart />
-        </ComponentExploration>
-        <ComponentExploration componentName="Messaging Card">
-          <MessagingCard
-            action={
-              <Pressable>
-                <Text color="fg" font="headline">
-                  Get started
+              }
+              media={
+                <Pictogram
+                  accessibilityLabel="Add to watchlist"
+                  dimension="48x48"
+                  name="usdcLoan"
+                />
+              }
+              mediaPlacement="end"
+              title={
+                <Text color="fg" font="body">
+                  Borrow USDC
                 </Text>
-              </Pressable>
-            }
-            description={
-              <Text color="fgMuted" font="body">
-                Get a USDC loan by using your Bitcoin as collateral
-              </Text>
-            }
-            media={
-              <Pictogram accessibilityLabel="Add to watchlist" dimension="48x48" name="usdcLoan" />
-            }
-            mediaPlacement="end"
-            title={
-              <Text color="fg" font="body">
-                Borrow USDC
-              </Text>
-            }
-            type="nudge"
-          />
-        </ComponentExploration>
-      </VStack>
+              }
+              type="nudge"
+            />
+          </ComponentExploration>
+          <ComponentExploration componentName="Different tokens per colorScheme">
+            <Avatar name="Test" size="xl" />
+          </ComponentExploration>
+        </VStack>
+      </ScrollView>
     </ComponentConfigProvider>
   );
 });
